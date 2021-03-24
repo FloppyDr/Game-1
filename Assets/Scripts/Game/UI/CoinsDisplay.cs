c213f6df12a49d341a5fab361c84e7de1bc7dd22
FloppyDr <DimaAlexandrov@yandex.ru>
2021-03-24 21:06:16 +0300

    Game created

---
+++ Assets/Scripts/Game/UI/CoinsDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinsDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _coinsDisplay;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {      
+        _player.CoinsChanged += OnCoinsChanged;
+    }
+    private void OnDisable()
+    {
+        
+        _player.CoinsChanged -= OnCoinsChanged;
+    }
+
+    private void OnCoinsChanged(int coins)
+    {
+        _coinsDisplay.text = coins.ToString();
+    }
+}
